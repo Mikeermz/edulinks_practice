6ff081387f928e5951110ff76a8dd29cf5956321
Mikee Ramirez <mikee.rmz@gmail.com>
2018-02-14 11:28:02 -0600

    Change scss to sass from sintaxis
    Signed-off-by: Mikee Ramirez <mikee.rmz@gmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp')
 var sass = require('gulp-sass')
 
 gulp.task('sass', () => {
-  return gulp.src('src/assets/styles/sass/estilo.scss')
+  return gulp.src('src/assets/styles/sass/estilo.sass')
     				 .pipe(sass().on('error', sass.logError))
     				 .pipe(gulp.dest('src/assets/styles/css'))
 })
