196b9173ddbcee3dc24f61f16eed3d8e56d76362
Mikee Ramirez <mikee.rmz@gmail.com>
2018-02-14 11:53:06 -0600

    clean all dist
    Signed-off-by: Mikee Ramirez <mikee.rmz@gmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
 var gulp = require('gulp')
 var sass = require('gulp-sass')
-var htmlmin = require('gulp-htmlmin');
+var htmlmin = require('gulp-htmlmin')
+var clean = require('gulp-clean')
+
 
 gulp.task('minify', () => {
   return gulp.src('src/*.html')
     				 .pipe(htmlmin({collapseWhitespace: true}))
-    				 .pipe(gulp.dest('dist'));
-});
+    				 .pipe(gulp.dest('dist'))
+})
 
 gulp.task('sass', () => {
   return gulp.src('src/assets/styles/sass/estilo.sass')
@@ -21,4 +23,9 @@ gulp.task('default', () => {
 gulp.task('copy', () => {
   return gulp.src(['src/assets/**/*', '!src/assets/styles/sass/'])
         		 .pipe(gulp.dest('dist/assets/'))
-});
+})
+
+gulp.task('clean', () => {
+  return gulp.src('dist/**', {read: false})
+        		 .pipe(clean());
+})
